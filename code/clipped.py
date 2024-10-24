e8553f13b21093d40343f97957b578ae75288f47
cristianionion <ion.cristian123@gmail.com>
2024-10-24 15:05:50 -0700

    added more to clipped

--- code/clipped.py
+++ code/clipped.py
@@ -29,5 +29,13 @@ wav.write("./code/sine.wav",sample_rate,sine_wave.astype(np.int16))
 ## PART 2
 
 # amplitude
-#amplitude = 1/2
+amplitude = 32767/2
 
+t = np.linspace(0.,1.,sample_rate)
+clipped_sine_wave = amplitude*np.sin(2.*np.pi*frequency*t)
+
+# https://numpy.org/devdocs/reference/generated/numpy.clip.html
+clipped_sine_wave = np.clip(clipped_sine_wave,-8192,8192)
+
+
+wav.write("./code/clipped.wav",sample_rate,clipped_sine_wave.astype(np.int16))
